0150fa5084ad24413441e6f51b576442bf04ceca
Xiaowei Chen <xiaoqianniu@gmail.com>
2024-03-20 12:49:16 -0400

    analyze and retrieve data with SQL

--- README.md
+++ README.md
@@ -5,4 +5,9 @@ Destination: 1. build a database
              3. connect PowerBI to a database
              4. visualize data in PowerBI
 
+and answer questions: 
+-- 1.Is our hotel revenue growing by year?
+-- 2. Should we increase our parking lot size?
+-- 3. What trends can we see in the data?
+
 Tools: SSMS, PowerBI, Git, Github
--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -6,4 +6,16 @@ select * from dbo.['2019$']
 union
 select * from dbo.['2020$']
 )
-select * from hotels;
+-- get hotel's revenue in each year
+/*select arrival_date_year,hotel,
+round(sum((stays_in_week_nights + stays_in_weekend_nights)* adr),2) as revenue 
+from hotels
+group by arrival_date_year,hotel;*/
+
+select * from hotels
+left join dbo.market_segment$
+on hotels.market_segment = dbo.market_segment$.market_segment
+left join dbo.meal_cost$
+on hotels.meal = dbo.meal_cost$.meal;
+
+
